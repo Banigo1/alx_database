dcaf45b14712290ff5cfa56100ac41e2a2875dc7
Precious Banigo <banigoprecious@yahoo.com>
2023-08-16 21:00:04 +0100

    ...

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,13 +1,11 @@
 /*3. List tables*/
 
 -- Get the database name from the command line argument
-database_name=$1
+database_name=mysql
 
 -- Connect to the MySQL server
 mysql -u root -p
 
--- Switch to the specified database
-USE $database_name;
 
 -- List all the tables in the database
 SHOW TABLES;
