a044b6188999b79398bd0799bb3e65675ec2ddf8
Precious Banigo <banigoprecious@yahoo.com>
2023-08-16 21:45:50 +0100

    ...

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -15,4 +15,19 @@ INSERT INTO hbtn_0c_0.second_table
     (3, 'Bob', 14),
     (4, 'George', 8);
 
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_9;
+USE hbtn_test_db_9;
+
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT PRIMARY KEY,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO second_table (id, name, score)
+VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
 
