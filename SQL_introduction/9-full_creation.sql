5d1f5fb7297593d1da05665e0f1dc7abcfe8ad62
Precious Banigo <banigoprecious@yahoo.com>
2023-08-16 23:33:37 +0100

    ...

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,33 +1,14 @@
 /*9. Full creation*/
 
 -- Create the table if it doesn't exist
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
-    id INT PRIMARY KEY,
-    name VARCHAR(256),
-    score INT
-);
-
--- Insert rows into the table
-INSERT INTO hbtn_0c_0.second_table 
-    (id, name, score) VALUES
-    (1, 'John', 10),
-    (2, 'Alex', 3),
-    (3, 'Bob', 14),
-    (4, 'George', 8);
-
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_9;
-USE hbtn_test_db_9;
-
+USE hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS second_table (
-    id INT PRIMARY KEY,
-    name VARCHAR(256),
-    score INT
+  id INT,
+  name VARCHAR(256),
+  score INT
 );
-
-INSERT INTO second_table (id, name, score)
-VALUES
-    (1, 'John', 10),
-    (2, 'Alex', 3),
-    (3, 'Bob', 14),
-    (4, 'George', 8);
-
+INSERT INTO second_table (id, name, score) VALUES 
+(1, 'John', 10), 
+(2, 'Alex', 3), 
+(3, 'Bob', 14), 
+(4, 'George', 8);
