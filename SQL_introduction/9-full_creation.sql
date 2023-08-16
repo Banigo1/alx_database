678821e2d48fe2fc0e1c79c689e870479f2fe25a
Precious Banigo <banigoprecious@yahoo.com>
2023-08-17 00:07:58 +0100

    ...

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -2,8 +2,8 @@
 
 -- Create the table if it doesn't exist
 
-USE hbtn_test_db_9;
-CREATE TABLE IF NOT EXISTS second_table 
+USE hbtn_0c_0;
+CREATE TABLE IF NOT EXISTS hbtn_test_db_9.second_table 
 (
   id INT, 
   name VARCHAR(256), 
