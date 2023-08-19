39ed77a9643dfbcd66e66ba46c6b3d6a93a7a185
Precious Banigo <banigoprecious@yahoo.com>
2023-08-19 13:18:47 +0100

    ...

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -4,5 +4,3 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-
-GRANT USAGE ON *.* TO `user_0d_2`@`localhost`
