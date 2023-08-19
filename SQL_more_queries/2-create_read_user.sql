7750779a49950197f208540f5286777e0c7171d7
Precious Banigo <banigoprecious@yahoo.com>
2023-08-19 14:00:47 +0100

    ...

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,6 @@
 -- create the mysql server user_0d_1
-
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
-GRANT SELECT TO 'user_0d_2'@'localhost';
+GRANT SELECT ON * TO 'user_0d_2'@'localhost';
